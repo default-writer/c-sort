d2b6601eb326a03946ce7c288b342345bc232c24
Artur Mustafin <artur.mustafin@gmail.com>
2020-12-23 10:22:23 +0000

    comared sort vs fast_sort

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
 #include <malloc.h>
 #include <time.h>
 #include <stdlib.h>
-#define length_of_main_array 5
+#include <string.h>
+
+#define length_of_main_array 50
 int function_of_filling_array(int, float*);
 
 void sort(float* ptr_One, float* ptr_Two);
+void fast_sort(float* ptr_One, float* ptr_Two);
 void swap(float* ptr_One, float* ptr_Two);
 
 int main() {
     srand(time(NULL));
     float* main_array = (float*)malloc(length_of_main_array * sizeof(float));
+    float* main_array2 = (float*)malloc(length_of_main_array * sizeof(float));
     *main_array = function_of_filling_array(length_of_main_array, main_array);
-
-    printf("\n");
+    
+    for (int i = 0; i < length_of_main_array; i++) {
+        main_array2[i] = main_array[i];
+    }
 
     for (int i = 0; i < length_of_main_array; i++) {
         printf("\n%4f", main_array[i]);
@@ -39,12 +45,19 @@ int main() {
     int n = length_of_main_array - 1;
     int i = 0;
 
-    // float test_array[5] = { -1.0f, -1.0f, 5.0f, -1.0f, -1.0f};
-
     sort(ptr_One, ptr_Two); 
 
     for (int i = 0; i < length_of_main_array; i++) {
         printf("\n%4f", main_array[i]);
+    }
+
+    printf("\n");
+    printf("\n");
+
+    fast_sort(&main_array2[0], &main_array2[length_of_main_array - 1]); 
+
+    for (int i = 0; i < length_of_main_array; i++) {
+        printf("\n%4f", main_array2[i]);
     }
 
     printf("\n");
@@ -57,6 +70,27 @@ void sort(float* ptr_One, float* ptr_Two)
         float* ptr_Tmp = ptr_One;
         while (ptr_One < ptr_Two)
         {
+            if (*ptr_One < *ptr_Two)
+                swap(ptr_One, ptr_Two);
+            ++ptr_One;
+        }
+        if (ptr_Tmp < ptr_Two) {
+            --ptr_Two;
+            ptr_One = ptr_Tmp;
+            continue;
+        }
+        break;
+    }
+}
+
+void fast_sort(float* ptr_One, float* ptr_Two)
+{
+    while (1)
+    {
+        float* ptr_Tmp = ptr_One;
+        while (ptr_One < ptr_Two)
+        {
+            // code for speed run (task actually is loosed constraints of strict sort order)
             if (*ptr_One > 0)
             {
                 ++ptr_One;
